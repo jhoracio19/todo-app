78708bb286a3ca221cc8c8c0569e0dc6141ceee5
Jose Horacio <jhoracio19@hotmail.com>
2026-02-01 09:10:13 -0600

    agregue la dependencia de uuid

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import "./style.css";
+import { App } from "./todos/app";
+import todoStore from "./store/todo-store";
 
-console.log("Hola mundo");
+todoStore.initStore();
+
+App("#app");
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,3 +1,5 @@
+import html from "./app.html?raw";
+
 /**
  *
  * @param {string} elementId
@@ -5,9 +7,11 @@
 
 export const App = (elementId) => {
   // Cuando la función App() se llama
-  () => {
+  (() => {
     const app = document.createElement("div");
-    app.innerHTML = "<h1>Hola Mundo</h1>";
+
+    app.innerHTML = html;
+
     document.querySelector(elementId).append(app);
-  };
+  })();
 };
