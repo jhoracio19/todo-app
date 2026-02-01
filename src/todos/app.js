034877103f686064e28dbd3c5f641de32b2390a3
Jose Horacio <jhoracio19@hotmail.com>
2026-02-01 08:47:55 -0600

    agregue la estructura inicial del proyecto

---
+++ src/todos/app.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {string} elementId
+ */
+
+export const App = (elementId) => {
+  // Cuando la función App() se llama
+  () => {
+    const app = document.createElement("div");
+    app.innerHTML = "<h1>Hola Mundo</h1>";
+    document.querySelector(elementId).append(app);
+  };
+};
