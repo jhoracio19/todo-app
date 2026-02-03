6c6835e18f4a4ac573c32cf6378426850611a970
Jose Horacio <jhoracio19@hotmail.com>
2026-02-02 18:39:15 -0600

    agregue funcionalidad para hacer persistente los datos en el localstorage

--- src/store/todo-store.js
+++ src/store/todo-store.js
@@ -18,12 +18,22 @@ const state = {
 };
 
 const initStore = () => {
-  console.log(state);
+  loadStore();
   console.log("InitStore");
 };
 
 const loadStore = () => {
-  throw new Error("Not implemented");
+  if (!localStorage.getItem("state")) return;
+
+  const { todos = [], filter = Filters.All } = JSON.parse(
+    localStorage.getItem("state"),
+  );
+  state.todos = todos;
+  state.filter = filter;
+};
+
+const saveStateToLocalStorage = () => {
+  localStorage.setItem("state", JSON.stringify(state));
 };
 
 const getTodos = (filter = Filters.All) => {
@@ -49,6 +59,8 @@ const addTodo = (description) => {
   if (!description) throw new Error("Description is required");
 
   state.todos.push(new Todo(description));
+
+  saveStateToLocalStorage();
 };
 
 const toggleTodo = (todoId) => {
@@ -58,18 +70,22 @@ const toggleTodo = (todoId) => {
     }
     return todo;
   });
+  saveStateToLocalStorage();
 };
 
 const deleteTodo = (todoId) => {
   state.todos = state.todos.filter((todo) => todo.id !== todoId);
+  saveStateToLocalStorage();
 };
 
 const deleteCompleted = () => {
-  state.todos = state.todos.filter((todo) => todo.done);
+  state.todos = state.todos.filter((todo) => !todo.done);
+  saveStateToLocalStorage();
 };
 
 const setFilter = (newFilter = Filters.All) => {
   state.filter = newfilter;
+  saveStateToLocalStorage();
 };
 
 const getCurrentFilter = () => {
--- src/todos/app.js
+++ src/todos/app.js
@@ -4,6 +4,7 @@ import todoStore from "../store/todo-store";
 import { renderTodos } from "./use-cases";
 
 const ElementIDs = {
+  ClearCompleted: ".clear-completed",
   TodoList: ".todo-list",
   NewTodoInput: "#new-todo-input",
 };
@@ -30,6 +31,9 @@ export const App = (elementId) => {
   // Referencias HTML
   const newDescriptionInput = document.querySelector(ElementIDs.NewTodoInput);
   const todoListUL = document.querySelector(ElementIDs.TodoList);
+  const clearCompletedButton = document.querySelector(
+    ElementIDs.ClearCompleted,
+  );
 
   // Listeners
   newDescriptionInput.addEventListener("keyup", (event) => {
@@ -54,6 +58,11 @@ export const App = (elementId) => {
     if (!element || !isDestroyElement) return;
 
     todoStore.deleteTodo(element.getAttribute("data-id"));
+    displayTodos();
+  });
+
+  clearCompletedButton.addEventListener("click", () => {
+    todoStore.deleteCompleted();
     displayTodos();
   });
 };
